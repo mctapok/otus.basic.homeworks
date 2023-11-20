5701cd107e9989cba4aa39594616427f2720a5be
mctapok <vektor.ct@gmail.com>
2023-11-20 21:00:16 +0300

    feat: hw 14 complete

---
+++ src/main/java/ru/gavrilov/basic/homework/lesson14/AppArrayDataException.java
@@ -0,0 +1,7 @@
+package ru.gavrilov.basic.homework.lesson14;
+
+public class AppArrayDataException extends RuntimeException{
+    public AppArrayDataException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/ru/gavrilov/basic/homework/lesson14/Main.java
@@ -0,0 +1,51 @@
+package ru.gavrilov.basic.homework.lesson14;
+
+public class Main {
+    public static void main(String[] args) {
+        String[][] array = {
+                {"1", "2", "1", "2"},
+                {"1", "2", "1", "2"},
+                {"1", "2", "1", "2"},
+                {"1", "4", "2", "2"},
+        };
+        getArray(array);
+        sumArr(array);
+//        try {
+//            sumArr(array);
+//        } catch (AppArrayDataException e) {
+//            System.out.println("data error " + e.getMessage());
+//        }
+    }
+
+    public static void getArray(String[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array.length != 4) {
+                throw new AppArraySizeException("bad array: column length != 4; column length = " + array.length);
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            String[] strings = array[i];
+            for (int j = 0; j < strings.length; j++) {
+                if (array[j].length != 4)
+                    throw new AppArraySizeException("bad array: line length != 4; " + "line " + (j + 1));
+            }
+        }
+        System.out.println("array accepted");
+    }
+
+    public static int sumArr(String[][] arr) throws AppArrayDataException {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(arr[i][j]);
+                } catch (RuntimeException e) {
+                    throw new AppArrayDataException("line " + (i + 1) + " column " + (j + 1));
+                }
+            }
+        }
+        System.out.println("array sum = " + sum);
+        return sum;
+    }
+}
